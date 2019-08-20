6aaa42363862ead2adab07c66d79709471cacce1
zhangyirong123 <525386154@qq.com>
2019-08-20 22:58:43 +0800

    增加主页面

--- src/views/Host.vue
+++ src/views/Host.vue
@@ -1,69 +1,87 @@
 <template>
-  <div id="app">
-    <div >
-      <div style="float: right">
-        <span>用户名：{{getUserName}}</span>
-        <button type="submit" style="margin-left:20px" v-on:click="_out">退出登录</button>
+  <div class="container">
+    <header class="header">
+      <div class="image">
+        <img alt="logo" src="../assets/header.jpg" style="width:50px;height: 50px;" />
       </div>
-      <br>
-      <br>
-      <div id="nav">
-        <router-link tag="a" :to="'/myJx'" >我的京西</router-link>|
-        <router-link tag="a" :to="'/myInfo'" >个人资料</router-link>|
-        <router-link to="/myAddress" >收货地址</router-link>
+      <div class="link">
+        <ul class="linkin">
+          <li style="display:inline;">
+            <a href="/Login">登录</a>
+          </li>
+          <li style="display:inline;">
+            <a href="/Register">注册</a>
+          </li>
+        </ul>
       </div>
-    </div>
-    <router-view/>
+    </header>
+    <main>
+      <img class="suancai" alt="suancai" src="../assets/suancai.jpg" />
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+    </main>
+    <footer>
+      <p>Here's some contact info</p>
+    </footer>
   </div>
 </template>
 
-
 <script>
+export default {
+  name: "App",
 
-  export default {
-
-    name: 'App',
-
-    data() {
-      return {
-        getUserName:"",
-      };
-    },
-
-    mounted() {
-      this.getUserName = sessionStorage.getItem("getUserName");
-    },
-
-    methods: {
-      _out:function() {
-        window.sessionStorage.clear();
-        console.log("退出登录，清空sessionStorage");
-        window.location.reload();
-      }
-    }
-  };
-
+  data() {
+    return {
+      getUserName: ""
+    };
+  }
+};
 </script>
 
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  #nav
-  padding 30px
-  border 1px solid #000
+.header {
+  align-items: center;
+  display: flex;
+  flex-wrap: nowrap;
+  background-color: #24292e;
+  color: hsla(0, 0%, 100%, 0.7);
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 9px;
+  z-index: 32;
+}
 
-  h1, h2
-    font-weight: normal
-  ul
-    list-style-type: none
-    padding: 0
-  li
-    display: inline-block
-    margin: 0 10px
-  a
-    color: #42b983
+.image {
+  position: relative;
+  left: 15px;
+  width: 50px;
+  height: 50px;
+}
+
+.link {
+  position: fixed;
+  right: 40px;
+}
+
+.linkin {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  padding-top: 6px;
+  padding-bottom: 6px;
+}
+
+a:link, a:visited {
+  font-weight: bold;
+  color: #FFFFFF;
+  text-align: center;
+  padding: 6px;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+a:hover, a:active {
+  background-color: #5E5E5E;
+}
+.suancai{}
 </style>
